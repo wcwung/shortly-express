5f8bcd5a17203ed13e018f99d542e41b6483cdf0
wcwung <williamcwung@gmail.com>
2015-03-02 19:45:57 -0800

    Add newUser event listener
    - TODO: Fix bugs in .on(sync/error).

--- public/client/signupLinkView.js
+++ public/client/signupLinkView.js
@@ -3,26 +3,36 @@ Shortly.signupLinkView = Backbone.View.extend({
 
   template: Templates['signup'],
 
-  // events: {
-  //   'submit': 'shortenUrl'
-  // },
+  events: {
+    'submit': 'newUser'
+  },
 
   render: function() {
     this.$el.html( this.template() );
     return this;
   },
 
+newUser: function(e){
+    var $username = this.$el.find('form #username');
+    var user = new Shortly.User({username: $username.val()});
+    user.on('sync', this.success, this);
+    user.on('error', this.failure, this);
+    console.log('new user submit button clicked');
+  },
+
   success: function(link) {
     this.stopSpinner();
-    var view = new Shortly.LinkView({ model: link });
+    var view = new Shortly.User({ model: user });
     this.$el.find('.message').append(view.render().$el.hide().fadeIn());
   },
 
   failure: function(model, res) {
     this.stopSpinner();
     this.$el.find('.message')
-      .html('Please enter a valid URL')
+      .html('Sorry, this username is taken.')
       .addClass('error');
     return this;
   }
+
+
 });
