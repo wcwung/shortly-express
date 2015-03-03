1a55fa00661ec7531c87854f00bd63ac1e373973
wcwung <williamcwung@gmail.com>
2015-03-02 19:34:38 -0800

    Remove bcrypt from User model

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,9 @@
 var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+
 });
 
 module.exports = User;
